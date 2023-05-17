07c650c1f7de6caab9e271ae757380e350e017bb
Bigben1611 <kutumov.sz@gmail.com>
2023-05-17 19:57:15 +0500

    Added main styles

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,63 @@
+<main class="main">
+<section class="s1">
+    <div class="about">
+        <h2 class="about__h2">Обо мне</h2>
+        <p class="about__p">Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
+    </div>
+</section>
+<section class="s2">
+    <div class="project">
+        <img src="numbers__item1.svg" alt="" class="project__img"/>
+        <p class="project__p">40+<br/>проектов</p>
+    </div>
+    <div class="project">
+        <img src="" alt="" class="project__img"/>
+        <p class="project__p">40+<br/>проектов</p>
+    </div>
+    <div class="project">
+        <img src="" alt="" class="project__img"/>
+        <p class="project__p">40+<br/>проектов</p>
+    </div>
+    <div class="project">
+        <img src="" alt="" class="project__img"/>
+        <p class="project__p">40+<br/>проектов</p>
+    </div>
+    <div class="project">
+        <img src="" alt="" class="project__img"/>
+        <p class="project__p">40+<br/>проектов</p>
+    </div>
+    <div class="project">
+        <img src="" alt="" class="project__img"/>
+        <p class="project__p">40+<br/>проектов</p>
+    </div>
+</section>
+<section class="s3 layer__1110">
+    <div class="s3__left">
+        <h2 class="s3__left__h2">Верстальщик</h2>
+        <p class="s3__left__name">Тиль Швайгер</p>
+        <p class="s3__left__p">Разрабатываю сайты для малого и среднего бизнеса. У меня можно заказать дизайн и верстку небольшого сайта или интернет-магазина</p>
+        <a href="#" class="s3__left__a">НАПИСАТЬ МНЕ</a>
+    </div>
+    <img src="hero-avatar.svg" alt="" class="s3__img"/>
+</section>
+<div class="s__bg"><section class="s4 layer__1110">
+    <div class="s4__work">
+        <h2 class="s4__work__h2">Как я работаю</h2>
+        <p class="s4__work__p">Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
+    </div>
+    <div class="s4__video"><img src="Vector.svg" alt=""/></div>
+</section></div >
+<div class="s__bg"><section class="s5">
+    <h2 class="s5__h2">Хотите веб-сайт?</h2>
+    <p class="s5__p">Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
+    <form action="" class="form"/>
+        <div class="form__div">
+            <input type="text" class="form__field" placeholder="Ваше имя">
+            <input type="text" class="form__field" placeholder="Ваш e-mail">
+        </div>
+            <textarea class="form__text form__field" placeholder="Сообщение"></textarea>
+        <button class="form__button">ОТПРАВИТЬ</button>
+    </form>
+</div>
+</section>
+</main>
